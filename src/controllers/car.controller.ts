dc0e593bd5b2ce0f946c8213bf201a62d3ef59e0
Riko Airlan Ramadhan <rikoairlan57@gmail.com>
2025-07-10 12:40:50 +0700

    fix create update data car in mongodb

--- src/controllers/car.controller.ts
+++ src/controllers/car.controller.ts
@@ -69,3 +69,30 @@ export const createCar = async (req: Request, res: Response): Promise<void> => {
       .json({ message: "Server error", error: error.message || error });
   }
 };
+
+export const updateCar = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    if (req.file) {
+      updateData.imageCars = req.file.path;
+    }
+
+    const updatedCar = await Car.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedCar) {
+      res.status(404).json({ message: "Car not found" });
+      return;
+    }
+
+    res.status(200).json(updatedCar);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: "Failed to update car", error: error.message });
+  }
+};
